a4083a4178226be9af3a538418d5bf652b45721a
betty <betty280303@gmail.com>
2022-04-04 13:50:32 -0500

    Diseño Ventana Sistema1, Botones

--- src/sistemapasteleria/Sistema.java
+++ src/sistemapasteleria/Sistema.java
@@ -42,16 +42,16 @@ public class Sistema extends javax.swing.JFrame {
         jButton1.setBackground(new java.awt.Color(0, 0, 0));
         jButton1.setFont(new java.awt.Font("Tahoma", 1, 20)); // NOI18N
         jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Pasteles");
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/1.jpg"))); // NOI18N
         getContentPane().add(jButton1);
         jButton1.setBounds(100, 210, 300, 60);
 
         jButton2.setBackground(new java.awt.Color(0, 0, 0));
         jButton2.setFont(new java.awt.Font("Tahoma", 1, 20)); // NOI18N
         jButton2.setForeground(new java.awt.Color(255, 255, 255));
-        jButton2.setText("Ventas");
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/2.jpg"))); // NOI18N
         getContentPane().add(jButton2);
-        jButton2.setBounds(100, 310, 300, 60);
+        jButton2.setBounds(100, 320, 300, 60);
 
         fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Diseño sin título (4).png"))); // NOI18N
         getContentPane().add(fondo);
